aa81e4f77ef8f3ed2eddb2bf6e7275f67aeb31e4
Amine <amine.benzina@lycee-faure.fr>
2023-03-31 15:25:46 +0200

    page nouvelle entrée

--- vue/vueNouvelleEntree.php
+++ vue/vueNouvelleEntree.php
@@ -1,2 +1,54 @@
-<h1>Nouvelle entrée</h1>
-<div class="row"
+<div class="text-center row"><h1>Nouvelle entrée</h1></div>
+<br><br><br><br>
+<div class="row">
+    <div class="col-lg-6 col-12">
+        <form>
+            <div class="form-group row">
+                <div class="col-lg-6 col-12">
+                    <label for="inputPassword" class="form-label">Nombre d'entrées adultes</label>
+                    <br><br><br>
+                    <label for="inputPassword" class="col-form-label">Nombre d'entrées enfants</label>
+                </div>
+                <div class="col-lg-6 col-12">
+                    <input type="text" class="form-control" id="inputPassword">
+                    <br><br>
+                    <input type="text" class="form-control" id="inputPassword">
+                </div>
+            </div>
+        </form>
+    </div>
+    <div class="col-lg-6 col-12">
+        <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+            <label class="form-check-label" for="flexCheckDefault">Exposition permanente</label>
+        </div>
+        <br>
+        <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+            <label class="form-check-label" for="flexCheckDefault">L'âge d'or du bronze</label>
+        </div>
+        <br>
+        <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+            <label class="form-check-label" for="flexCheckDefault">Les relations familiales chez les Chuzzbek du moyen âge</label>
+        </div>
+        <br>
+        <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+            <label class="form-check-label" for="flexCheckDefault">Tenues vestimentaires des péruviens du 17ème siècle</label>
+        </div>
+    </div>
+</div>
+<br><br>
+<div class="row">
+    <div class="text-center">
+        <button class="btn btn-outline-primary" type="submit">Calculer tarif</button>
+        <button class="btn btn-outline-success" type="submit">Valider l'entrée</button>
+    </div>
+</div>
+<br><br>
+<div class="row">
+    <div class="text-center">
+        Total à payer :
+    </div>
+</div>
